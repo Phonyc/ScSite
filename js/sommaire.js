45887432ab3765ca1d625dd1f7dc673dfcc19f29
Phonyc <119613433+Phonyc@users.noreply.github.com>
2024-06-01 22:48:13 +0200

    Optimisation Chromium + ajout fonctions utilisables + conteneurs pages explications

--- js/sommaire.js
+++ js/sommaire.js
@@ -1,7 +1,4 @@
 
-// let curve_transition_card = `((-400 - x)*(-350 - x)*(-200 - x)*(-100+x)*(80 + x)*(200 - x)*(250 - x))/(1.858e17*${window.innerWidth})`;
-//
-
 function goToCard1(card) {
      if (card.id === "sommaireCard2") {
          card.style.top = "-44vh"
@@ -10,13 +7,14 @@ function goToCard1(card) {
          card.style.top = "-86vh"
          card.style.right = "0"
      }
-    card.classList.add("clickedCard");
+    document.querySelectorAll(".sommaireCard").forEach((tpcard) => {
+        tpcard.classList.add("unclickedCard")
+    });
+     card.classList.add("clickedCard");
 
     setTimeout(() => {
         window.scrollTo(0, 2 * window.innerHeight);
     }, 1000)
-    setTimeout(() => {
-        card.classList.remove("clickedCard");
-    }, 2000)
-
+        // TODO Remove clickedCardClass en revenant
 }
+
